ca4dbe96ad46f515131a790bcc771f07664188b9
Lennart Rittel <33598633+lrittel@users.noreply.github.com>
2025-07-24 22:40:17 +0200

    feat: add an option to apply a layout to the currently focused workspace
    Extent the layout file format with the `focused_workspace` key.
    
    Closes #4.

--- src/sway_out/main.py
+++ src/sway_out/main.py
@@ -8,7 +8,7 @@ import yaml
 from i3ipc import Connection
 
 from .applications import launch_applications_from_layout
-from .connection import check_replies
+from .connection import check_replies, get_focused_workspace
 from .layout import create_layout, has_matching_layout
 from .layout_files import load_layout_configuration
 
@@ -35,11 +35,32 @@ def main_apply(ctx: click.Context, layout_file):
         click.echo(f"Failed to read layout configuration: {e}", err=True)
         return
 
+    focused_workspace = get_focused_workspace(connection)
+
     for name, content in configuration.workspaces.items():
         replies = connection.command(f'workspace "{name}')
         check_replies(replies)
         launched_applications = launch_applications_from_layout(connection, content)
         create_layout(connection, name, content, launched_applications)
+
+    if configuration.focused_workspace:
+        if focused_workspace is None:
+            click.echo(
+                "No focused workspace found. Cannot apply focused workspace layout.",
+                err=True,
+            )
+            return
+        replies = connection.command(f'workspace "{focused_workspace}"')
+        check_replies(replies)
+        launched_applications = launch_applications_from_layout(
+            connection, configuration.focused_workspace
+        )
+        create_layout(
+            connection,
+            focused_workspace,
+            configuration.focused_workspace,
+            launched_applications,
+        )
 
 
 @main.command("check")
@@ -53,6 +74,8 @@ def main_check(ctx: click.Context, layout_file):
     except (yaml.YAMLError, pydantic.ValidationError) as e:
         click.echo(f"Failed to read layout configuration: {e}", err=True)
         return
+
+    focused_workspace = get_focused_workspace(connection)
 
     is_matching = True
     tree = connection.get_tree()
@@ -69,6 +92,25 @@ def main_check(ctx: click.Context, layout_file):
         else:
             click.echo(f"Workspace '{name}' does not match the layout.")
             is_matching = False
+
+    if configuration.focused_workspace:
+        if focused_workspace is None:
+            click.echo(
+                "No focused workspace found. Cannot check focused workspace layout.",
+                err=True,
+            )
+            return
+        for workspace in tree.workspaces():
+            if workspace.name == name:
+                break
+        else:
+            assert False, "Focused workspace should always be in the tree"
+        if has_matching_layout(workspace, configuration.focused_workspace):
+            click.echo("Focused workspace matches its layout.")
+        else:
+            click.echo("Focused workspace does not match its layout.")
+            is_matching = False
+
     if is_matching:
         click.echo("All workspaces match their layouts.")
     else:
