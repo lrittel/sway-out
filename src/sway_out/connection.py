269cc6da4ae962a7d0cdd5e3001868bcb8f4463e
Lennart Rittel <33598633+lrittel@users.noreply.github.com>
2025-07-25 01:21:16 +0200

    refactor: extract the "find con with con_id" functionality into a function
    Related to #14.

--- src/sway_out/connection.py
+++ src/sway_out/connection.py
@@ -39,6 +39,7 @@ def check_replies(replies: list[CommandReply]):
 
 def get_focused_workspace(connection: Connection) -> Con | None:
     """Get the currently focused workspace.
+
     Arguments:
         connection: The Sway connection.
 
@@ -57,3 +58,32 @@ def get_focused_workspace(connection: Connection) -> Con | None:
         logger.warning("No focused workspace found.")
         return None
     return focused
+
+
+def find_cons_by_id(connection: Connection, *con_ids: int) -> tuple[Con, ...]:
+    """Finds all containers with the given IDs.
+
+    Each call to this function results in one IPC call. So passing multiple
+    con_ids at once reduces the number of IPC calls while also ensuring that
+    all con objects are consistent.
+
+    Arguments:
+        connection: The Sway connection to use.
+        con_ids: The IDs of the containers to find.
+
+    Returns:
+        A list of containers with the given IDs. The order of the list matches
+        the order of the IDs.
+
+    Raises:
+        RuntimeError: If a container with a given ID is not found in the tree.
+    """
+    tree = connection.get_tree()
+    result = tuple(tree.find_by_id(con_id) for con_id in con_ids)
+    missing_ids = [con_id for con_id, con in zip(con_ids, result) if con is None]
+    if missing_ids:
+        raise RuntimeError(
+            f"Container(s) with IDs {', '.join(str(i) for i in missing_ids)} not found in tree"
+        )
+    assert None not in result
+    return result
