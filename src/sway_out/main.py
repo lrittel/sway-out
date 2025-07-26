df66eff27afd8e7b18b264c7e440f01bfe342a47
Lennart Rittel <33598633+lrittel@users.noreply.github.com>
2025-07-26 03:09:29 +0200

    feat: add a notification
    Show a notification while applying layouts.
    
    Closes #5.

--- src/sway_out/main.py
+++ src/sway_out/main.py
@@ -1,10 +1,16 @@
 """Main entrypoint."""
 
 import logging
+from contextlib import ExitStack
+from dataclasses import dataclass
 
 import click
+import gi
 import pydantic
 import yaml
+
+gi.require_version("Notify", "0.7")
+from gi.repository import Notify
 from i3ipc import Connection
 
 from .applications import launch_applications_from_layout
@@ -13,25 +19,43 @@ from .layout import create_layout
 from .layout_files import load_layout_configuration, map_workspaces
 from .marks import apply_marks
 from .matching import find_current_workspace
+from .notifications import progress_notification
 
 logger = logging.getLogger(__name__)
 
+PROG_NAME = "sway-out"
+
+Notify.init(PROG_NAME)
+
+
+@dataclass
+class GlobalState:
+    """The user-provided configuration for the application and some global state."""
+
+    connection: Connection
+    notifications: bool = True
+
 
 @click.group()
-@click.version_option(prog_name="sway-out", package_name="sway-out")
+@click.version_option(prog_name=PROG_NAME, package_name="sway-out")
+@click.option(
+    "--notifications/--no-notifications",
+    default=True,
+    help="Enable or disable notifications.",
+)
 @click.pass_context
-def main(ctx: click.Context):
+def main(ctx: click.Context, notifications: bool):
     """Main entrypoint."""
 
     logging.basicConfig(level=logging.DEBUG)
-    ctx.ensure_object(Connection)
+    ctx.obj = GlobalState(Connection(), notifications)
 
 
 @main.command("apply")
 @click.argument("layout_file", type=click.File("rb"))
 @click.pass_context
 def main_apply(ctx: click.Context, layout_file):
-    connection = ctx.find_object(Connection)
+    connection: Connection = ctx.obj.connection
     assert connection is not None
     try:
         configuration = load_layout_configuration(layout_file)
@@ -41,18 +65,24 @@ def main_apply(ctx: click.Context, layout_file):
 
     workspace_layout_mapping = map_workspaces(connection, configuration)
 
-    for workspace_name, workspace_layout in workspace_layout_mapping.items():
-        run_command(connection, f"workspace {workspace_name}")
-        workspace_con = find_current_workspace(connection)
-        workspace_layout._con_id = workspace_con.id
-        assert workspace_con is not None, "No current workspace found?"
-        logger.info("Applying layout for workspace: %s", workspace_name)
-        launch_applications_from_layout(connection, workspace_layout)
-        create_layout(connection, workspace_layout)
-        apply_marks(connection, workspace_layout)
-    logger.info(
-        f"Successfully applied layout for {len(workspace_layout_mapping)} workspace(s)"
-    )
+    with progress_notification("Applying layout", "Workspace") as notification:
+        if ctx.obj.notifications:
+            notification.start()
+        for index, (workspace_name, workspace_layout) in enumerate(
+            workspace_layout_mapping.items()
+        ):
+            notification.update(index + 1, len(workspace_layout_mapping))
+            run_command(connection, f"workspace {workspace_name}")
+            workspace_con = find_current_workspace(connection)
+            workspace_layout._con_id = workspace_con.id
+            assert workspace_con is not None, "No current workspace found?"
+            logger.info("Applying layout for workspace: %s", workspace_name)
+            launch_applications_from_layout(connection, workspace_layout)
+            create_layout(connection, workspace_layout)
+            apply_marks(connection, workspace_layout)
+        logger.info(
+            f"Successfully applied layout for {len(workspace_layout_mapping)} workspace(s)"
+        )
 
 
 if __name__ == "__main__":
